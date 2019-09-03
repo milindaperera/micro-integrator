0fe80649f6247ddc90231f75d1a90e257236cab0
Milinda Perera <milindaperera@Milindas-MacBook-Pro.local>
2019-09-03 10:05:01 +0530

    Add startup finalizer component and startlevels

--- components/org.wso2.micro.integrator.startup/src/main/java/org/wso2/micro/integrator/startup/StartupFinalizerServiceComponent.java
+++ components/org.wso2.micro.integrator.startup/src/main/java/org/wso2/micro/integrator/startup/StartupFinalizerServiceComponent.java
@@ -1,19 +1,24 @@
 /*
- * Copyright 2004,2005 The Apache Software Foundation.
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-package org.wso2.micro.integrator.core.internal;
+package org.wso2.micro.integrator.startup;
 
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.context.ConfigurationContext;
@@ -37,6 +42,7 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 import org.wso2.carbon.CarbonConstants;
 import org.wso2.micro.core.ServerStatus;
+import org.wso2.micro.integrator.core.internal.CarbonCoreDataHolder;
 import org.wso2.micro.integrator.core.services.Axis2ConfigurationContextService;
 
 import java.util.ArrayList;
@@ -53,7 +59,7 @@ import java.util.TimerTask;
  * fact  that requests from external parties should only be serviced after the Axis2 engine
  * & Carbon has  reached a stable and consistent state.
  */
-@Component(name = "org.wso2.micro.integrator.core.internal.StartupFinalizerServiceComponent", immediate = true)
+@Component(name = "org.wso2.micro.integrator.startup.StartupFinalizerServiceComponent", immediate = true)
 public class StartupFinalizerServiceComponent implements ServiceListener {
     private static final Log log = LogFactory.getLog(StartupFinalizerServiceComponent.class);
     private static final String START_TIME = "wso2carbon.start.time";
